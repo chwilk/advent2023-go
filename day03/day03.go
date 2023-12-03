e2f1e82918be835159b9e6799265317f85f5b237
chwilk <chwilk@gmail.com>
2023-12-03 12:39:02 -0600

    Passes first half test case

--- day03/day03.go
+++ day03/day03.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 type Day03 struct {
@@ -23,21 +24,19 @@ func (f Day03) Run(input io.Reader, part int) (result string) {
 }
 
 func PartA(lines io.Reader) (answer int) {
-	scanner := bufio.NewScanner(lines)
-	var schematic [][]byte
-	for scanner.Scan() {
-		line := scanner.Bytes()
-		schematic = append(schematic, line)
-	}
+	schematic := readPuzzle(lines)
+
 	re := regexp.MustCompile(`(\d+)`)
-	for _, row := range schematic {
+	for i, row := range schematic {
 		matches := re.FindAllIndex(row, -1)
 		for _, m := range matches {
 			tmp, err := strconv.Atoi(string(row[m[0]:m[1]]))
 			if err != nil {
-				panic (err)
+				panic(err)
 			}
-			answer += tmp
+			if checkAdjacent(schematic, i, m) {
+				answer += tmp
+			}
 		}
 	}
 	return
@@ -49,4 +48,58 @@ func PartB(lines io.Reader) (answer int) {
 	for scanner.Scan() {
 	}
 	return
+}
+
+func readPuzzle(lines io.Reader) (puzzle [][]byte) {
+	scanner := bufio.NewScanner(lines)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		for i, v := range line {
+			switch {
+			case v == '.':
+				line[i] = 0
+			case v > 47 && v < 58:
+				// noop
+			default:
+				line[i] = 'S'
+			}
+		}
+		puzzle = append(puzzle, line)
+	}
+	return
+}
+
+func checkAdjacent(data [][]byte, row int, cols []int) bool {
+	lastRow := len(data) - 1
+	maxCols := len(data[0])
+
+	checkRange := [2][2]int{{0,0},{0,0}}
+	if row == 0 {
+		checkRange[0][1] = 0
+		checkRange[1][1] = 1
+	} else if row == lastRow {
+		checkRange[0][1] = row -1
+		checkRange[1][1] = lastRow
+	} else {
+		checkRange[0][1] = row - 1
+		checkRange[1][1] = row + 1
+	}
+	if cols[0] == 0 {
+		checkRange[0][0] = 0
+		checkRange[1][0] = cols[1]
+	} else if cols[1] == maxCols {
+		checkRange[0][0] = cols[0] - 1
+		checkRange[1][0] = maxCols - 1
+	} else {
+		checkRange[0][0] = cols[0] - 1
+		checkRange[1][0] = cols[1]
+	}
+
+	for i := checkRange[0][1]; i <= checkRange[1][1]; i++ {
+		if strings.Contains(string(data[i][checkRange[0][0]:checkRange[1][0]+1]), "S") {
+			return true
+		}
+	}
+
+	return false
 }
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -2,6 +2,7 @@ package day03
 
 import (
 	"advent2023-go/helpers"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -31,5 +32,72 @@ func TestRun(t *testing.T) {
 	})
 }
 
+func TestReadPuzzle(t *testing.T) {
+	expect := [][]byte{
+		{'4', '6', '7', 0, 0, '1', '1', '4', 0, 0},
+		{0, 0, 0, 'S', 0, 0, 0, 0, 0, 0},
+		{0, 0, '3', '5', 0, 0, '6', '3', '3', 0},
+		{0, 0, 0, 0, 0, 0, 'S', 0, 0, 0},
+		{'6', '1', '7', 'S', 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 'S', 0, '5', '8', 0},
+		{0, 0, '5', '9', '2', 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, '7', '5', '5', 0},
+		{0, 0, 0, 'S', 0, 'S', 0, 0, 0, 0},
+		{0, '6', '6', '4', 0, '5', '9', '8', 0, 0}}
+	got := readPuzzle(strings.NewReader(example))
+	if !reflect.DeepEqual(expect, got) {
+		t.Errorf("Expected %v, got %v", expect, got)
+	}
+}
+
 func TestCheckAdjacent(t *testing.T) {
+	t.Run("Easy case, adjacent symbol", func(t *testing.T) {
+		data := [][]byte{
+			{0, 0, 0, 0, 0},
+			{0, '4', '4', '4', 0},
+			{0, 0, 0, 0, 'S'}}
+		got := checkAdjacent(data, 1, []int{1, 4})
+		if got == false {
+			t.Error("Missed adjacent symbol")
+		}
+	})
+	t.Run("Edge case, top", func(t *testing.T) {
+		data := [][]byte{
+			{0, '4', '4', '4', 0},
+			{0, 0, 0, 0, 'S'}}
+		got := checkAdjacent(data, 0, []int{1, 4})
+		if got == false {
+			t.Error("Missed adjacent symbol")
+		}
+	})
+	t.Run("Edge case, bottom", func(t *testing.T) {
+		data := [][]byte{
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 'S'},
+			{0, '4', '4', '4', 0}}
+		got := checkAdjacent(data, 2, []int{1, 4})
+		if got == false {
+			t.Error("Missed adjacent symbol")
+		}
+	})
+	t.Run("Edge case, bottom left", func(t *testing.T) {
+		data := [][]byte{
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 'S', 0},
+			{'4', '4', '4', 0, 0}}
+		got := checkAdjacent(data, 2, []int{0, 3})
+		if got == false {
+			t.Error("Missed adjacent symbol")
+		}
+	})
+	t.Run("Edge case, top right", func(t *testing.T) {
+		data := [][]byte{
+			{0,0,'4', '4', '4'},
+			{0, 'S', 0, 0, 0},
+			{0, 0, 0, 0, 0}}
+		got := checkAdjacent(data, 0, []int{2, 5})
+		if got == false {
+			t.Error("Missed adjacent symbol")
+		}
+	})
 }
