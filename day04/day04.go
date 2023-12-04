a08bfaf0924e161809996c22ee8a203f009b1f4e
chwilk <chwilk@gmail.com>
2023-12-04 14:33:47 -0600

    Part A solved

--- day04/day04.go
+++ day04/day04.go
@@ -5,9 +5,18 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 type Day04 struct {
+}
+
+type Card struct {
+	id      int
+	have    []int
+	winning []int
+	points  int
 }
 
 func (f Day04) Run(input io.Reader, part int) (result string) {
@@ -22,8 +31,9 @@ func (f Day04) Run(input io.Reader, part int) (result string) {
 
 func PartA(lines io.Reader) (answer int) {
 	scanner := bufio.NewScanner(lines)
-
 	for scanner.Scan() {
+		card:= NewCard(scanner.Text())
+		answer += card.points
 	}
 	return
 }
@@ -34,4 +44,61 @@ func PartB(lines io.Reader) (answer int) {
 	for scanner.Scan() {
 	}
 	return
+}
+
+func NewCard(row string) (card Card) {
+	tokens := strings.Split(row, " ")
+	var half bool
+	for _, tok := range tokens[1:] {
+		switch tok {
+		case "|":
+			half = true
+		case "":
+			// noop if double spaces
+		default:
+			var err error
+			length := len(tok)
+			if tok[length-1] == ':' {
+				card.id, err = strconv.Atoi(tok[0:length-1])
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				var tmp int
+				tmp, err = strconv.Atoi(tok)
+				if err == nil {
+					switch half {
+					case false:
+						card.have = append(card.have, tmp)
+					default:
+						card.winning = append(card.winning, tmp)
+					}
+				}
+			}
+		}
+	}
+	card.points = scoreCard(card)
+	return card
+}
+
+func scoreCard(card Card) (score int) {
+	for _, num := range card.have {
+		if  contains(card.winning, num) {
+			if score > 0 {
+				score *= 2
+			} else {
+				score = 1
+			}
+		}
+	}
+	return
+}
+
+func contains(a []int, val int) bool {
+	for _, v := range a {
+		if v == val {
+			return true
+		}
+	}
+	return false
 }
--- day04/day04_test.go
+++ day04/day04_test.go
@@ -2,15 +2,21 @@ package day04
 
 import (
 	"advent2023-go/helpers"
+	"reflect"
 	"strings"
 	"testing"
 )
 
-const example string = ``
+const example string = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 
 func TestRun(t *testing.T) {
 	t.Run("Run function part A", func(t *testing.T) {
-		expect := "0"
+		expect := "13"
 		got := Day04{}.Run(strings.NewReader(example), helpers.PartA)
 		helpers.CheckString(t, expect, got)
 	})
@@ -19,4 +25,27 @@ func TestRun(t *testing.T) {
 		got := Day04{}.Run(strings.NewReader(example), helpers.PartB)
 		helpers.CheckString(t, expect, got)
 	})
+}
+
+func TestNewCard(t *testing.T) {
+	expect := Card{id: 1, have: []int{1}, winning: []int{1,20}, points: 1}
+	got := NewCard("Card  1:  1 |  1 20")
+	if ! reflect.DeepEqual(expect, got) {
+		t.Errorf("Expected %v, got %v", expect, got) 
+	}
+}
+
+func TestScoreCard(t *testing.T) {
+	data := Card{id: 1, have: []int{1}, winning: []int{1,20}}
+	got := scoreCard(data)
+	expect := 1
+	helpers.Check(t, expect, got)
+}
+
+func TestContains(t *testing.T) {
+	a := []int{1, 2, 3, 4}
+	v := 3
+	if ! contains(a,v) {
+		t.Errorf("contains function could not find %v in %v", v, a)
+	}
 }
